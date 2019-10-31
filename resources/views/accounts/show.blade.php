e391570a4cbc0fe32791f53cb188b90fc3f7294a
mark-mokaya <mark.mokaya@strathmore.edu>
2019-10-31 09:36:21 +0300

    css

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.main')
 @section('content')
 
-<div style="max-width: 1060px; min-width: 300px; margin: 20px auto; border: 5px solid #000;">
+<div style="width: 1060px; margin: 20px auto; border: 5px solid #000;">
     <h1>{{strToUpper($Account->acc_name)}} ACCOUNT</h1>
     <h2>Remaining: <b style="color: #2ecc71;">Ksh. {{$Account->amount}}</b></h2>
     <p>{{ucfirst($Account->description)}}<br>
     <a href="/accounts">{{"<"}} Go Back</a></p>
 </div>
 
-<div style="max-width: 500px; min-width: 290px; max-height: 600px; padding: 30px 0px; margin: 20px 10px; border: 5px solid #000; display:inline-block; overflow: hidden;">
+<div style="width: 500px; height: 600px; padding: 30px 0px; margin: 20px 10px; border: 5px solid #000; display:inline-block; overflow: hidden;">
 	
         <h1>EXPENSES</h1>
     <article>
-			<div style="max-width: 500px; overflow:hidden; margin: 50px auto;">
+			<div style="width: 500px; overflow:hidden; margin: 50px auto;">
 				<canvas id="doughChart" width="10px"></canvas>    
 			</div>       
 	</article>
 </div> 
 
-<div style="max-width: 500px; min-width: 290px; max-height: 600px; padding: 30px 0px; margin: 20px 10px; border: 5px solid #000; display:inline-block; overflow: hidden;">
+<div style="width: 500px; height: 600px; padding: 30px 0px; margin: 20px 10px; border: 5px solid #000; display:inline-block; overflow: hidden;">
 	<h1>RECORDS</h1>  
     <article style="height: 72%; overflow-y: auto;">
         @foreach ($Expenses as $Expense)
