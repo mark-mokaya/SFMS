f212f23fa1f2b7f8b6c39f0a7aa7573803b59e4e
mark-mokaya <mark.mokaya@strathmore.edu>
2019-11-03 00:29:51 +0300

    Revert "hope sijamess"
    This reverts commit 9c319bc4f9c32bfb1d9e3913a337817921699ac1.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,10 +39,9 @@ class LoginController extends Controller
         $this->middleware('guest:admin')->except('logout');
     }
 
-    public function username(){
-        return 'username';
-    }
-
+    // public function username(){
+    //     return 'username';
+    // }
     public function showAdminLoginForm()
     {
         return view('auth.login', ['url' => 'admin']);
@@ -51,15 +50,15 @@ class LoginController extends Controller
     public function adminLogin(Request $request)
     {
         $this->validate($request, [
-            'username'   => 'required|username',
+            'email'   => 'required|email',
             'password' => 'required|min:6'
     ]);
 
-        if (Auth::guard('admin')->attempt(['username' => $request->username, 'password' => $request->password], $request->get('remember'))) {
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
 
         return redirect()->intended('/admin');
         }
-        return back()->withInput($request->only('username', 'remember'));
+        return back()->withInput($request->only('email', 'remember'));
 
 }
 }
